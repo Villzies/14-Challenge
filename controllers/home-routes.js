4d5db88309633446828da74b394b5f71f322c4eb
Villzies <gemberling.stephan@att.net>
2023-11-24 12:21:51 -0600

    Deleted homepage route from home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,4 @@
-router.get("/", withAuth, async (req, res) => {
-    try {
-      const result = await fetchAndDisplayArticles();
-      res.render("homepage", { result, logged_in: req.session.logged_in });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+
   
   //Checks if the user is logged in. If not, redirects to the login page. 
   router.get("/login", (req, res) => {

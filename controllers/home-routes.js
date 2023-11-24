8b609949266430be92a80401dc167a4670ddc5f3
Villzies <gemberling.stephan@att.net>
2023-11-24 17:14:42 -0600

    Messed around with home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 require("dotenv").config();
+const withAuth = require("../utils/auth");
   
+router.get("/", withAuth, async (req, res) => {
+    try {
+      res.render("homepage", { result, logged_in: req.session.logged_in });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
   //Checks if the user is logged in. If not, redirects to the login page. 
   router.get("/login", (req, res) => {
     if (req.session.logged_in) {

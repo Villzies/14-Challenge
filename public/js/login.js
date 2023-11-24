a2e8ab9eafe25f5d73a70151f71e1e2edbd8e395
Villzies <gemberling.stephan@att.net>
2023-11-24 10:58:21 -0600

    Added signup functionality

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    //Checks username and password using the login api route
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  //If the username and password match, go to the default page. Otherwise, alerts the user that they failed to login
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);

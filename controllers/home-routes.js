8cae0208cdc376cd3330b552b67f475d17b977c0
Villzies <gemberling.stephan@att.net>
2023-11-24 12:23:31 -0600

    Added back home routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,28 @@
+router.get("/", withAuth, async (req, res) => {
+    try {
+      const result = await fetchAndDisplayArticles();
+      res.render("homepage", { result, logged_in: req.session.logged_in });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //Checks if the user is logged in. If not, redirects to the login page. 
+  router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+  
+  //Checks if the user is already logged in. If not, redirects to the signup page.
+  router.get("/signup", (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("signup");
+  });

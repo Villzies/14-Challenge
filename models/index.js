f514b359b15a5dc558d2c57a783253242be733af
Villzies <gemberling.stephan@att.net>
2023-11-24 18:08:09 -0600

    Added post relationships

--- models/index.js
+++ models/index.js
@@ -12,6 +12,21 @@ User.hasMany(Comment, {
   onDelete: "CASCADE",
 });
 
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
 Comment.belongsTo(User, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
